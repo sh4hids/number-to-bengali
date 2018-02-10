1ce70c096fa1076e6be8dcfa2bc7ab88c372a193
5h4hid5 <shahid.bracu@gmail.com>
2018-02-10 20:13:04 +0600

    Added main file with test

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# bengali-number
+# Number to Bengali
+
+Convert any number (English) to Bengali with this tiny node module.
+
+## Install
+
+```bash
+npm install -S bengali-number
+```
+
+```bash
+yarn add bengali-number
+```
+
+## Use
+
+```javascript
+const toBengaliNum = require('bengali-number');
+
+toBengaliNum(123) // returns '১২৩'
+```
+
+## Test
+Test this module with jest
---
+++ index.test.js
@@ -0,0 +1,18 @@
+var toBengaliNum = require('./index');
+var msg = 'Invalid input type';
+
+test('1 should be converted to ১', () => {
+  expect(toBengaliNum(1)).toBe('১');
+});
+
+test('"123" should be converted to ১২৩', () => {
+  expect(toBengaliNum('123')).toBe('১২৩');
+});
+
+test('1.2 should be converted to ১.২', () => {
+  expect(toBengaliNum(1.2)).toBe('১.২');
+});
+
+test('1.a should return to ' + msg, () => {
+  expect(toBengaliNum('1.a')).toBe(msg);
+});
